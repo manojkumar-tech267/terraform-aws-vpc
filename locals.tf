088c5e4d91b87140b5ab1d6086a03899e2ea4cb3
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2026-04-24 08:53:59 +0530

    vpc module updated

--- locals.tf
+++ locals.tf
@@ -9,5 +9,13 @@ locals {
     {
       Name = "${var.project}-${var.environment}"
     },
-    var.vpc_tags)
+    var.vpc_tags
+    )
+    igw_final_tags = merge(
+    local.common_tags,
+    {
+    Name = "${var.project}-${var.environment}"
+    },
+    var.igw_tags
+    )
 }
