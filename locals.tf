1e25e28f74e6e7f6b43b866323717bb85cf6c429
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2026-04-26 21:29:41 +0530

    vpc module updated

--- locals.tf
+++ locals.tf
@@ -19,4 +19,39 @@ locals {
     var.igw_tags
     )
     az_names = slice(data.aws_availability_zones.available.names,0,2)
+    public_route_table_final_tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-public"
+    },
+    var.public_route_table_tags
+  )
+  private_route_table_final_tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-private"
+    },
+    var.private_route_table_tags
+  )
+  database_route_table_final_tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-database"
+    },
+    var.database_route_table_tags
+  )
+  elastic_ip_final_tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-nat"
+    },
+    var.elastic_ip_tags
+  )
+  nat_gateway_final_tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}"
+    },
+    var.nat_gateway_tags
+  )
 }
--- variables.tf
+++ variables.tf
@@ -30,3 +30,48 @@ variable "public_subnet_tags" {
     type = map 
     default = {}
 }
+
+variable "private_subnet_cidrs" {
+    type = list(string)
+    default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+
+variable "private_subnet_tags" {
+    type = map 
+    default = {}
+}
+
+variable "database_subnet_cidrs" {
+    type = list(string)
+    default = ["10.0.21.0/24","10.0.22.0/24"]
+}
+
+variable "database_subnet_tags" {
+    type = map 
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    type = map 
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    type = map 
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    type = map 
+    default = {}
+}
+
+variable "elastic_ip_tags" {
+    type = map 
+    default = {}
+}
+
+variable "nat_gateway_tags" {
+    type = map 
+    default = {}
+}
